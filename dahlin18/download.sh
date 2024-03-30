f1bef78ca665f3cfbfd09ec70ab9f2c52e061ed5
Mervin Fansler <mmfansler@gmail.com>
2024-03-30 19:54:10 -0400

    add dahlin18

---
+++ dahlin18/download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p bam
+
+wget -O bam/lsk_sigab1.bam 'https://sra-pub-src-1.s3.amazonaws.com/SRR6348971/LSK_SLX11508_SIGAB1_possorted_genome_bam.bam.1'
+wget -O bam/lsk_sigac1.bam 'https://sra-pub-src-1.s3.amazonaws.com/SRR6348972/LSK_SLX11508_SIGAC1_possorted_genome_bam.bam.1'
+wget -O bam/lsk_sigad1.bam 'https://sra-pub-src-1.s3.amazonaws.com/SRR6348973/LSK_SLX11508_SIGAD1_possorted_genome_bam.bam.1'
+wget -O bam/lk_sigaf1.bam 'https://sra-pub-src-1.s3.amazonaws.com/SRR6348974/Lin_cKit__SLX11508_SIGAF1_possorted_genome_bam.bam.1'
+wget -O bam/lk_sigag1.bam 'https://sra-pub-src-1.s3.amazonaws.com/SRR6348975/Lin_cKit__SLX11508_SIGAG1_possorted_genome_bam.bam.1'
+wget -O bam/lk_sigah1.bam 'https://sra-pub-src-1.s3.amazonaws.com/SRR6348976/Lin_cKit__SLX11508_SIGAH1_possorted_genome_bam.bam.1'
